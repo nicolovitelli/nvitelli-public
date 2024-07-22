093ed155ad2282c35e16678e235648016029e63a
nicolovitelli <nicolo.vitelli1@gmail.com>
2024-07-22 12:19:34 +0200

    Quartz sync: Jul 22, 2024, 12:19 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,9 +30,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
   ],
 }
 
